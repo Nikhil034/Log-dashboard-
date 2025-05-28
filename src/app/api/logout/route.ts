23e6a1510e431257d52fc3704e6014898f2bc2ae
Nikhil034 <nikhilsondarava@lampros.tech>
2025-05-28 16:59:30 +0530

    Change into API when user logout from dashboard.

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -3,13 +3,10 @@ import { NextResponse } from "next/server";
 export async function GET() {
   const response = NextResponse.json({ message: "Logged out" });
   
-
-  // Clear cookies
-  response.cookies.set("token", "", { path: "/", expires: new Date(0) });
+  response.cookies.set("auth-token", "", { path: "/", expires: new Date(0) });
   response.cookies.set("role", "", { path: "/", expires: new Date(0) });
   response.cookies.set("user_email", "", { path: "/", expires: new Date(0) });
   console.log("Logout response: ", response);
-
 
   return response;
 }
